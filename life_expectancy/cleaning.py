2a2fc0b3578cf2710663493bc49c655e0df50c22
MeloDuarte <duarte.ro.melo@nos.pt>
2023-04-20 14:16:31 +0100

    add refactoring

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -2,19 +2,43 @@
 
 import argparse
 import pandas as pd
+from pandas import DataFrame
 
-def clean_data(country):
+
+def load_data() -> DataFrame:
     """
-    Loads `eu_life_expectancy_raw.tsv` dataframe and cleans it.
-    Save the resulting dataframe to `pt_life_expectancy.csv`.
+    Loads `eu_life_expectancy_raw.tsv` DataFrame.
+    :return: DataFrame loaded.
     """
-    expectancy_w = pd.read_csv(
+    data = pd.read_csv(
         "life_expectancy/data/eu_life_expectancy_raw.tsv",
         sep='\t',
         header=0
     )
 
-    expectancy_lg = pd.melt(expectancy_w, id_vars=expectancy_w.columns[0], var_name="year")
+    return data
+
+
+def save_data(expectancy_result: DataFrame) -> None:
+    """
+    Save the resulting DataFrame to `pt_life_expectancy.csv`.
+    :param expectancy_result: DataFrame cleaned to be saved.
+    """
+    expectancy_result.to_csv(
+        'life_expectancy/data/pt_life_expectancy.csv',
+        index=False
+    )
+
+
+def clean_data(expectancy_wide: DataFrame, country: str) -> DataFrame:
+    """
+    Unpivots the DataFrame to long format, and cleans data.
+    :param expectancy_wide: expectancy DataFrame in wide format.
+    :param country: region where to filter data.
+    :return: cleaned DataFrame
+    """
+
+    expectancy_lg = pd.melt(expectancy_wide, id_vars=expectancy_wide.columns[0], var_name="year")
 
     expectancy_lg[['unit', 'sex', 'age', 'region']] = pd.DataFrame(
         expectancy_lg[expectancy_lg.columns[0]].str.split(',', expand=True),
@@ -29,10 +53,23 @@ def clean_data(country):
     expectancy_pt = expectancy_clean[expectancy_clean['region'] == country]
 
     expectancy_result = expectancy_pt[['unit', 'sex', 'age', 'region', 'year', 'value']]
-    expectancy_result.to_csv('life_expectancy/data/pt_life_expectancy.csv', index=False)
+
+    return expectancy_result
+
+
+def main(country: str='PT') -> None:
+    """Loads, cleans and saves data.
+    :param country: region where to filter data. 
+    """
+    expectancy_w = load_data()
+    expectancy_result = clean_data(expectancy_w, country)
+    save_data(expectancy_result)
+
 
 if __name__ == "__main__":  # pragma: no cover
+    # Country command-line option. (the default should be `PT`)
     parser = argparse.ArgumentParser()
     parser.add_argument("-country", default="PT")
     args = parser.parse_args()
-    clean_data(args.country)
+
+    main(args.country)

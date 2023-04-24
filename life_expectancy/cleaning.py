7c64f53eb26ea528072f2c870648b4fefbcaaa47
MeloDuarte <duarte.ro.melo@nos.pt>
2023-04-24 11:46:57 +0100

    improve code flexibility and readability

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,6 +1,7 @@
 """Module Assignment 1: EU Life Expectancy"""
 
 import argparse
+from pathlib import Path
 import pandas as pd
 from pandas import DataFrame
 
@@ -10,8 +11,11 @@ def load_data() -> DataFrame:
     Loads `eu_life_expectancy_raw.tsv` DataFrame.
     :return: DataFrame loaded.
     """
+    # Gives the parent dir location of the file `pt_life_expectancy.csv`
+    filename = Path(__file__).parent / "data/eu_life_expectancy_raw.tsv"
+    print(filename)
     data = pd.read_csv(
-        "life_expectancy/data/eu_life_expectancy_raw.tsv",
+        filename,
         sep='\t',
         header=0
     )
@@ -24,15 +28,22 @@ def save_data(expectancy_result: DataFrame) -> None:
     Save the resulting DataFrame to `pt_life_expectancy.csv`.
     :param expectancy_result: DataFrame cleaned to be saved.
     """
+    # Gives the parent dir location of the file `pt_life_expectancy.csv`
+    filename = Path(__file__).parent / "data/pt_life_expectancy.csv"
+    print(filename)
+
     expectancy_result.to_csv(
-        'life_expectancy/data/pt_life_expectancy.csv',
+        filename,
         index=False
     )
 
 
-def clean_data(expectancy_wide: DataFrame, country: str) -> DataFrame:
+def clean_data(expectancy_wide: DataFrame, country: str = 'PT') -> DataFrame:
     """
-    Unpivots the DataFrame to long format, and cleans data.
+    Unpivots the DataFrame to long format, ensures `year` is an `int` 
+    (with the appropriate data cleaning if required), ensures `value` 
+    is a `float` (with the appropriate data cleaning if required, 
+    and do remove the NaNs) and filters the data by region.
     :param expectancy_wide: expectancy DataFrame in wide format.
     :param country: region where to filter data.
     :return: cleaned DataFrame
@@ -45,14 +56,19 @@ def clean_data(expectancy_wide: DataFrame, country: str) -> DataFrame:
         index=expectancy_lg.index
     )
 
-    expectancy_clean = expectancy_lg[expectancy_lg['year'].str.strip().str.isdigit()]
+    expectancy_region = expectancy_lg[expectancy_lg['region'].str.upper() == country.upper()]
+
+    expectancy_clean = expectancy_region[expectancy_region['year'].str.strip().str.isdigit()]
+
+    # Find all occurrences of floats in column `value`
+    # and converts argument to numeric type. Returns NaN otherwise.
     value_floats = expectancy_clean['value'].str.findall(r"\-?\d+\.\d+")
     expectancy_clean['value'] = pd.to_numeric(value_floats[value_floats.str.len() == 1].str[0])
-    expectancy_clean = expectancy_clean.dropna()
 
-    expectancy_pt = expectancy_clean[expectancy_clean['region'] == country]
+    expectancy_clean = expectancy_clean.dropna(subset=['year', 'value'])
 
-    expectancy_result = expectancy_pt[['unit', 'sex', 'age', 'region', 'year', 'value']]
+
+    expectancy_result = expectancy_clean[['unit', 'sex', 'age', 'region', 'year', 'value']]
 
     return expectancy_result
 
@@ -61,15 +77,15 @@ def main(country: str='PT') -> None:
     """Loads, cleans and saves data.
     :param country: region where to filter data. 
     """
-    expectancy_w = load_data()
-    expectancy_result = clean_data(expectancy_w, country)
-    save_data(expectancy_result)
+    expectancy = load_data()
+    expectancy_cleaned = clean_data(expectancy, country)
+    save_data(expectancy_cleaned)
 
 
 if __name__ == "__main__":  # pragma: no cover
     # Country command-line option. (the default should be `PT`)
     parser = argparse.ArgumentParser()
-    parser.add_argument("-country", default="PT")
+    parser.add_argument("-country", default="PT", help="Filters data by `country`")
     args = parser.parse_args()
 
     main(args.country)
